df3b0c8a5a0316c8a25b1a771c25e48fd1f466cc
Robert Garner <rgarner011235@gmail.com>
2024-10-08 17:38:27 -0600

    Add project files.

---
+++ HRManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HRManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HRManager/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace HRManager
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
