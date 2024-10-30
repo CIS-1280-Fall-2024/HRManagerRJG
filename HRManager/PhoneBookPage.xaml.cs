0a90a8c48cd2cc3f583695d1de07d4f9b24607b4
Robert Garner <rgarner011235@gmail.com>
2024-10-29 19:00:54 -0600

    Added departments

--- HRManager/PhoneBookPage.xaml.cs
+++ HRManager/PhoneBookPage.xaml.cs
@@ -1,12 +1,21 @@
+using HRManager.Models;
 using HRManager.Services;
 
 namespace HRManager;
 
 public partial class PhoneBookPage : ContentPage
 {
+	private EmployeeService employeeService;
+
 	public PhoneBookPage()
 	{
 		InitializeComponent();
 		PhoneBook phoneBook = new PhoneBook();
+		employeeService = new EmployeeService();
+		foreach (Employee employee in employeeService.Employees)
+		{
+			phoneBook.PhoneBookEntries.Add(employee);
+		}
+		PhoneBookLabel.Text = phoneBook.GetPhoneBook();
 	}
 }
---
+++ HRManager/Services/DepartmentsService.cs
@@ -0,0 +1,16 @@
+﻿using HRManager.Models;
+
+namespace HRManager.Services
+{
+    public class DepartmentsService
+    {
+        public List<Department> Departments { get; set; };
+
+        public DepartmentsService() 
+        {
+            Department dep = new Department("Sales Department");
+            Departments = new List<Department>();
+            Departments.Add(dep);                
+        }
+    }
+}
