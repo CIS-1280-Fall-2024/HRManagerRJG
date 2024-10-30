3ddb619e5a486e87b6b50d6a3835f0d5d773e1d5
Robert Garner <rgarner011235@gmail.com>
2024-10-29 19:02:26 -0600

    Added departments to phone book

--- HRManager/PhoneBookPage.xaml.cs
+++ HRManager/PhoneBookPage.xaml.cs
@@ -6,16 +6,25 @@ namespace HRManager;
 public partial class PhoneBookPage : ContentPage
 {
 	private EmployeeService employeeService;
+	private DepartmentsService departmentsService;
 
 	public PhoneBookPage()
 	{
 		InitializeComponent();
 		PhoneBook phoneBook = new PhoneBook();
 		employeeService = new EmployeeService();
+		departmentsService = new DepartmentsService();
+
 		foreach (Employee employee in employeeService.Employees)
 		{
 			phoneBook.PhoneBookEntries.Add(employee);
 		}
+
+		foreach (Department department in departmentsService.Departments)
+		{
+			phoneBook.PhoneBookEntries.Add(department);
+		}
+
 		PhoneBookLabel.Text = phoneBook.GetPhoneBook();
 	}
 }
